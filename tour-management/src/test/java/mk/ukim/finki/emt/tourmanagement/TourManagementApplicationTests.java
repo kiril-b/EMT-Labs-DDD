e6d9b163bef4795de7f6753ebc6e397dff3260a9
Kiril Blazevski <blazevskikiril@hotmail.com>
2023-05-17 00:46:35 +0200

    first commit

---
+++ tour-management/src/test/java/mk/ukim/finki/emt/tourmanagement/TourManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.tourmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
